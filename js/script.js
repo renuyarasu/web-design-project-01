52fab6be764c7bc18ab4e81e649f7d09cae46768
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-05 17:36:54 +0530

    Project 01: JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // Web Design - Project 01
 
+const menu_btn = document.querySelector('.menu_icon');
+menu_btn.addEventListener('click',()=>{
+    const menu = document.querySelector('.nav__links');
+    const menuIcon = document.querySelector('.menu_icon');
+    menu.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+})
